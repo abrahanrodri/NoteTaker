6db5720e756eeaaf74c85b3984e82e7d821a2cc4
Abe <abrahan.rodri@gmail.com>
2019-08-06 16:11:31 -0400

    updating the routes

--- data/seeds.sql
+++ data/seeds.sql
@@ -5,5 +5,9 @@ VALUES
   ("Note Taker App", "Complete this homework by Tuesday, August 6, 2019 11:59 PM"),
   ("READ THE DOCS", "Read documentation about... Node, Express, MySQL, etc."),
   ("Mark myself present in Bootcampspot", "Remember to mark myself present every Monday-Friday at 10am"),
-  ("Commit more", "Get those commits up. It shows the process and progress.")
+  ("Commit more", "Get those commits up. It shows the process and progress."),
+  
   -- ADD MORE
+  
+  ("ROBOTS", "Those darn robots"),
+  ("CATS", "do they love us?")
